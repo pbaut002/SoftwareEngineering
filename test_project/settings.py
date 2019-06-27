5bb194e5e12839d44c9a36d5795473a7244264bd
Peter Bautista <petergbautista@gmail.com>
2019-06-27 15:40:47 -0700

    Added PostgreSQL support

--- test_project/settings.py
+++ test_project/settings.py
@@ -11,12 +11,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -77,17 +79,18 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2' ,
-        'NAME': 'cpsc362',
-        'USER': 'peter',
-        'PASSWORD': 'bautista',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
-}
+DATABASES = {}
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2' ,
+    #     'NAME': 'cpsc362',
+    #     'USER': 'peter',
+    #     'PASSWORD': 'bautista',
+    #     'HOST':'localhost',
+    #     'PORT':'5432',
+    # }
+import dj_database_url
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
