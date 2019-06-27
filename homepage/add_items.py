92f85e8db2a35d18ee61bec006a150bcab92f7be
Peter Bautista <petergbautista@gmail.com>
2019-06-26 20:19:50 -0700

    Initial commit

---
+++ homepage/add_items.py
@@ -0,0 +1,11 @@
+Python 3.7.2 (default, Jan 29 2019, 19:41:29) 
+[GCC 7.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+(InteractiveConsole)
+>>> 
+KeyboardInterrupt
+>>> 
+KeyboardInterrupt
+>>> 
+KeyboardInterrupt
+>>> 
